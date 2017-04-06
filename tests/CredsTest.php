a4b0f5b750e8bca4deb60ca3ed813b3e68c8fae5
Lucas Green <lucas.m.green@gmail.com>
2017-04-06 14:56:19 -0700

    GetHostePage

--- tests/CredsTest.php
+++ tests/CredsTest.php
@@ -2,14 +2,17 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Mythril\AuthorizeNet\API;
+use Mythril\AuthorizeNet\Authenticate;
+
 $cfg = include(__DIR__ . '/../config.php');
 
-$api = new Mythril\AuthorizeNet\API(
+$api = new API(
 	$cfg['name'],
 	$cfg['tk'],
 	$cfg['sandbox']
 );
 
-$result = $api->execute(new Mythril\AuthorizeNet\Authenticate());
+$result = $api->execute(new Authenticate());
 
 var_dump($result);
